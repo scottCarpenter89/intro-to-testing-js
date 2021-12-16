d853ac4d27e21bfd99d538bb990f7cf5dec570ea
Scott Carpenter <carpenter.scott22@gmail.com>
2021-12-15 21:04:13 -0600

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -18,13 +18,13 @@ describe('sayHello', function (){
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function() {
-        expect(typeof sayHello()).toBe('string');
-    })
-    it('should return the string "Hello, name!" when executed', function () {
-        expect(sayHello()).toBe('Hello, name!');
-    });
-    it('should never return "undefined" when called', function () {
-        expect(sayHello()).not.toBe(undefined);
-    });
+    // it('should return a string when called', function() {
+    //     expect(typeof sayHello()).toBe('string');
+    // })
+    // it('should return the string "Hello, name!" when executed', function () {
+    //     expect(sayHello()).toBe('Hello, name!');
+    // });
+    // it('should never return "undefined" when called', function () {
+    //     expect(sayHello()).not.toBe(undefined);
+    // });
 });
